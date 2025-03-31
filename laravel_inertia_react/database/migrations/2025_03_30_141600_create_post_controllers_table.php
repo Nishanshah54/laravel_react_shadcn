83016949816f7ba05096f0ccb9c8e845721731a7
Nishanshah9 <142131567+Nishanshah54@users.noreply.github.com>
2025-03-31 14:32:30 +0545

    inital commit

---
+++ laravel_inertia_react/database/migrations/2025_03_30_141600_create_post_controllers_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_controllers', function (Blueprint $table) {
+            $table->id();
+            $table->String('post');
+            $table->String('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_controllers');
+    }
+};
